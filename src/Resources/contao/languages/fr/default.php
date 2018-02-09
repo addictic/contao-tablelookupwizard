4342b366c576b530ce73f0a7a3cc33fe59b92178
Aethys256 <aethys256@users.noreply.github.com>
2018-02-09 10:01:20 +0100

    Fix french languages

--- src/Resources/contao/languages/fr/default.php
+++ src/Resources/contao/languages/fr/default.php
@@ -5,7 +5,7 @@
  *
  * Copyright (C) 2013 terminal42 gmbh
  *
- * @package    TableLookupLinks
+ * @package    TableLookupWizard
  * @link       http://www.terminal42.ch
  * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
  */
@@ -15,7 +15,7 @@
 /**
  * Extension folder
  */
-$GLOBALS['TL_LANG']['MSC']['tlwNoJs']           = "Activez javascript dans votre navigateur, s'il-vous-plaît.";
+$GLOBALS['TL_LANG']['MSC']['tlwNoJs']           = "Veuillez activer javascript dans votre navigateur.";
 $GLOBALS['TL_LANG']['MSC']['tlwJsAlternative']  = 'Cliquez ici pour utiliser la version de secours de Javascript.';
 $GLOBALS['TL_LANG']['MSC']['tlwNoValue']        = "Rien n'a été sélectionné. Utilisez le champ de recherche pour trouver les enregistrements.";
 $GLOBALS['TL_LANG']['MSC']['tlwNoResults']      = 'Aucun résultat pour la recherche "%s".';
--- src/Resources/contao/languages/fr/modules.php
+++ src/Resources/contao/languages/fr/modules.php
@@ -5,7 +5,7 @@
  *
  * Copyright (C) 2013 terminal42 gmbh
  *
- * @package    TableLookupLinks
+ * @package    TableLookupWizard
  * @link       http://www.terminal42.ch
  * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
  */
